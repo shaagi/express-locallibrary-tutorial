8344da2a7f417300eb099aa49c62837612677d71
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-04-18 18:44:40 -0400

    connect mongoose code, make model files (empty) and wrote code for the author model

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://dbUser:<dbUserPassword>@cluster0.logwwes.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
